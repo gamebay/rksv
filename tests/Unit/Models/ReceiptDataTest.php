473f91a643387a61d7d39ecc968aa42183ee622f
Sandro Lovnički <sandro@scipack.xyz>
2019-10-15 21:29:06 +0200

    tests/: [add] Unit/Models/ReceiptDataTest

---
+++ tests/Unit/Models/ReceiptDataTest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+use \Gamebay\RKSV\Models\ReceiptData;
+
+class ReceiptDataTest extends TestCase
+{
+    public function testIsValidItem()
+    {
+        $receiptData = new ReceiptData();
+
+        $item =  [
+            'net' => 100,
+            'tax' => 20
+        ];
+        $this->assertTrue($receiptData->isValidItem($item));
+
+        $items = [
+            'net' => 100,
+            'tax' => 101
+        ];
+        $this->assertFalse($receiptData->isValidItem($items));
+
+        $item = [
+            'tax' => 90
+        ];
+        $this->assertFalse($receiptData->isValidItem($item));
+    }
+}
