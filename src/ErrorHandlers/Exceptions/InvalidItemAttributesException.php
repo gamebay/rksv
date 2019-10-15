e66d0de286941141719109ac6f4d2f9f7d5549a8
Sandro Lovnički <sandro@scipack.xyz>
2019-10-15 12:41:00 +0200

    src/: [add] ErrorHandlers/Exceptions/*

---
+++ src/ErrorHandlers/Exceptions/InvalidItemAttributesException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace ErrorHandlers\Exceptions;
+
+
+class InvalidItemAttributesException extends RksvException
+{
+
+}
---
+++ src/ErrorHandlers/Exceptions/RksvException.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace ErrorHandlers\Exceptions;
+
+
+class RksvException extends \Exception
+{
+    public function __construct($message, $code = 0, $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return $this->code . ": " . __CLASS__ . ": " . $this->message . "\n";
+    }
+}
