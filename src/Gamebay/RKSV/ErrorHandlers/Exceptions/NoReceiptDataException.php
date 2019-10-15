04dd00709f1d52678761f267133bc1cd4d27eb5c
Nino Korent <nino.korent@gamebay.io>
2019-10-15 15:33:10 +0200

    added comment blocks to exceptions

--- src/Gamebay/RKSV/ErrorHandlers/Exceptions/NoReceiptDataException.php
+++ src/Gamebay/RKSV/ErrorHandlers/Exceptions/NoReceiptDataException.php
@@ -4,7 +4,22 @@
 namespace Gamebay\RKSV\ErrorHandlers\Exceptions;
 
 
+use Illuminate\Http\Response;
+
+/**
+ * Class NoReceiptDataException
+ * @package Gamebay\RKSV\ErrorHandlers\Exceptions
+ */
 class NoReceiptDataException extends RksvException
 {
-
+    /**
+     * NoReceiptDataException constructor.
+     * @param string $message
+     * @param int $code
+     * @param null $previous
+     */
+    public function __construct($message = "Tried to instantiate DataReceipt without valid data", $code = Response::HTTP_UNPROCESSABLE_ENTITY, $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
--- src/Gamebay/RKSV/ErrorHandlers/Exceptions/RksvException.php
+++ src/Gamebay/RKSV/ErrorHandlers/Exceptions/RksvException.php
@@ -4,13 +4,26 @@
 namespace Gamebay\RKSV\ErrorHandlers\Exceptions;
 
 
+/**
+ * Class RksvException
+ * @package Gamebay\RKSV\ErrorHandlers\Exceptions
+ */
 class RksvException extends \Exception
 {
+    /**
+     * RksvException constructor.
+     * @param $message
+     * @param int $code
+     * @param null $previous
+     */
     public function __construct($message, $code = 0, $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return $this->code . ": " . __CLASS__ . ": " . $this->message . "\n";
