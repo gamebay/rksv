2dc2342d7732265885ae99b4d1636a81af5c16a6
Sandro Lovnički <sandro@scipack.xyz>
2019-10-17 00:48:40 +0200

    {Services}/: [add] Encrypter

---
+++ src/Gamebay/RKSV/Services/Encrypter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Gamebay\RKSV\Services;
+
+
+/**
+ * Class Encrypter
+ * @package Gamebay\RKSV\Services
+ */
+class Encrypter
+{
+    public static function encryptSalesCounter(string $salesCounter)
+    {
+        // TODO
+        $encryptedSalesCounter = $salesCounter;
+
+        return $encryptedSalesCounter;
+    }
+}
