e0f7db0699cdea36589e5765d351523debeb0e48
itapai <ksem.eks@gmail.com>
2023-11-09 12:38:56 +0100

    fix: NullSignService - bad null item type

--- src/Gamebay/RKSV/Services/SignServices/NullSignService.php
+++ src/Gamebay/RKSV/Services/SignServices/NullSignService.php
@@ -14,37 +14,40 @@ use Gamebay\RKSV\Validators\SignatureType;
  */
 class NullSignService extends BaseSignService implements SignServiceInterface
 {
-    /**
-     * NullSignService constructor.
-     * @param PrimeSignProvider $provider
-     * @param ReceiptData $receiptData
-     * @param string $encryptionKey
-     * @param string $tokenKey
-     * @param array $taxRates
-     * @param string $locationId
-     * @throws InvalidItemException
-     */
-    public function __construct(
-        PrimeSignProvider $provider,
-        ReceiptData $receiptData,
-        string $encryptionKey,
-        string $tokenKey,
-        array $taxRates,
-        string $locationId
-    ) {
-        parent::__construct($provider, $receiptData, $encryptionKey, $tokenKey, $taxRates, $locationId);
+  /**
+   * NullSignService constructor.
+   * @param PrimeSignProvider $provider
+   * @param ReceiptData $receiptData
+   * @param string $encryptionKey
+   * @param string $tokenKey
+   * @param array $taxRates
+   * @param string $locationId
+   * @throws InvalidItemException
+   */
+  public function __construct(
+    PrimeSignProvider $provider,
+    ReceiptData $receiptData,
+    string $encryptionKey,
+    string $tokenKey,
+    array $taxRates,
+    string $locationId
+  ) {
+    parent::__construct($provider, $receiptData, $encryptionKey, $tokenKey, $taxRates, $locationId);
 
-        $salesCounterCode = SignatureType::SIGN_CODE[ReceiptSigner::NULL_SIGN_TYPE];
+    $salesCounterCode = SignatureType::SIGN_CODE[ReceiptSigner::NULL_SIGN_TYPE];
 
-        $nullItem = [
-            'brutto' => 0,
-            'tax' => 20,
-        ];
+    $nullItem = [
+      [
+        'brutto' => 0,
+        'tax' => 20,
+      ]
+    ];
 
-        $this->receiptData = $receiptData;
+    $this->receiptData = $receiptData;
 
-        $this->receiptData->setItems($nullItem);
-        $this->receiptData->setSalesCounter($salesCounterCode);
-        $this->receiptData->setPreviousReceiptSignature($this->receiptData->getCashboxId());
-    }
+    $this->receiptData->setItems($nullItem);
+    $this->receiptData->setSalesCounter($salesCounterCode);
+    $this->receiptData->setPreviousReceiptSignature($this->receiptData->getCashboxId());
+  }
 }
+
